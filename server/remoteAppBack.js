3b879f846a3713223e88d4a76baaa10cf907410b
marc.foletto <marc.foletto@gmail.com>
2021-02-03 19:01:26 +0100

    chore(remoteAppBack): remove unecessary fct

--- server/remoteAppBack.js
+++ server/remoteAppBack.js
@@ -15,28 +15,6 @@ if (process.env.NODE_ENV !== 'production') {
   console.info('Your running your app in production mode')
 }
 
-/**
- * Normalize a port into a number, string, or false.
- */
-function normalizePort(val) {
-  const port = parseInt(val, 10)
-
-  if (Number.isNaN(port)) {
-    // named pipe
-    return val
-  }
-
-  if (port >= 0) {
-    // port number
-    return port
-  }
-
-  return false
-}
-
-/**
- * Module dependencies.
- */
 const app = require('./app')
 // const debug = require('debug')('remoteAppStandalone:server');
 const http = require('http')
@@ -44,7 +22,7 @@ const http = require('http')
 /**
  * Get port from environment and store in Express.
  */
-const port = normalizePort(process.env.PORT || '8085')
+const port = process.env.PORT || '8085'
 app.set('port', port)
 
 /**
@@ -52,43 +30,26 @@ app.set('port', port)
  */
 const server = http.createServer(app)
 
-/**
- * Event listener for HTTP server "error" event.
- */
-function onError(error) {
+server.listen(port)
+server.on('error', error => {
   if (error.syscall !== 'listen') {
     throw error
   }
 
-  const bind = typeof port === 'string' ? `Pipe ${port}` : `Port ${port}`
-
   // handle specific listen errors with friendly messages
   switch (error.code) {
     case 'EACCES':
-      console.error(`${bind} requires elevated privileges`)
+      console.error(`Port: ${port} requires elevated privileges`)
       process.exit(1)
       break
     case 'EADDRINUSE':
-      console.error(`${bind} is already in use`)
+      console.error(`Port: ${port} is already in use`)
       process.exit(1)
       break
     default:
       throw error
   }
-}
-
-/**
- * Event listener for HTTP server "listening" event.
- */
-function onListening() {
-  const addr = server.address()
-  const bind = typeof addr === 'string' ? `pipe ${addr}` : `port ${addr.port}`
-  console.log(`Listening on ${bind}`)
-}
-
-/**
- * Listen on provided port, on all network interfaces.
- */
-server.listen(port)
-server.on('error', onError)
-server.on('listening', onListening)
+})
+server.on('listening', () =>
+  console.log(`Listening on port ${server.address().port}`)
+)
